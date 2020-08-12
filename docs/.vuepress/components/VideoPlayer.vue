f10cc869d791509be04c844d9dce83b44c9b659a
StarlightUnion <wang2387574095@outlook.com>
2020-08-12 22:33:10 +0800

    modify: update videoplayer 2020/08/12 22:33:06

---
+++ docs/.vuepress/components/VideoPlayer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="video-player">
+    <video
+      controls="controls"
+      :src="src"
+      :poster="poster"
+    >
+      Your browser does not support the video tag.
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayBar',
+  props: {
+    src: { type: String },
+    poster: { type: String }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .video-player
+    width 100%
+    height 100%
+    video
+      width 100%
+      height 100%
+      outline none
+      background-color #000
+</style>
